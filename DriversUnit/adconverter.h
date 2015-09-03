d2c0768b4fb721f5d4f0ebb1e7cef475815a1195
Lukaszm94 <Lukaszm6@gmail.com>
2015-09-03 22:36:46 +0200

    Add thermometer reading

--- DriversUnit/adconverter.h
+++ DriversUnit/adconverter.h
@@ -3,19 +3,30 @@
 
 #include <avr/io.h>
 
+namespace ADConverterSettings
+{
+	float referenceVoltage;
+	uint16_t maxValue;
+};
+
 class ADConverter
 {
 public:
 	ADConverter()
 	{
+		
+	}
+	
+	static void init()
+	{
 		ADMUX |= (1<<REFS0); //ref is AVcc
 		ADCSRA |= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0); //enable ADC, 128div factor
 		
-		referenceVoltage = 5.0; //V
-		maxValue = 1024;
+		ADConverterSettings::referenceVoltage = 5.0; //V
+		ADConverterSettings::maxValue = 1024;
 	}
 	
-	uint16_t  getRawMeasurement(uint8_t channel)
+	uint16_t static getRawMeasurement(uint8_t channel)
 	{
 		setChannel(channel);
 		startConversion();
@@ -24,7 +35,7 @@ public:
 	}
 	
 	//offset is SUBTRACTED from every raw reading!
-	uint32_t getRawSum(uint8_t channel, int samplesCount, int16_t offset = 0) 
+	uint32_t static getRawSum(uint8_t channel, int samplesCount, int16_t offset = 0) 
 	{
 		uint32_t sum = 0;
 		for(int i =0; i < samplesCount; i++) {
@@ -40,13 +51,13 @@ public:
 		return sum;
 	}
 	
-	float getVoltage(uint8_t channel)
+	float static getVoltage(uint8_t channel)
 	{
 		uint16_t raw = getRawMeasurement(channel);
 		return convertRawMeasurementToVoltage(raw);
 	}
 	
-	float getAverageVoltage(uint8_t channel, int numberOfSamples) {
+	float static getAverageVoltage(uint8_t channel, int numberOfSamples) {
 		unsigned long sum = 0;
 		for(int i = 0; i < numberOfSamples; i++) {
 			sum += getRawMeasurement(channel);
@@ -56,24 +67,23 @@ public:
 	}
 private:
 
-	inline void setChannel(uint8_t channel)
+	inline static void setChannel(uint8_t channel)
 	{
 		ADMUX = (ADMUX & 0xF0) | channel; //set appropiate channel
 	}
-	inline void startConversion()
+	inline static void startConversion()
 	{
 		ADCSRA |= (1<<ADSC); //start conversion
 	}
-	inline bool isCoversionFinished()
+	inline static bool isCoversionFinished()
 	{
 		return !(ADCSRA & (1<<ADSC)); //checking AD Start Conversion flag, which is set to 1 if conversion is pending
 	}
-	float convertRawMeasurementToVoltage(uint16_t rawReading)
+	float static convertRawMeasurementToVoltage(uint16_t rawReading)
 	{
-		return (float)rawReading/maxValue*referenceVoltage;
+		return (float)rawReading/ADConverterSettings::maxValue*ADConverterSettings::referenceVoltage;
 	}
 
-	float referenceVoltage;
-	uint16_t maxValue;
+
 };
 #endif
--- DriversUnit/main.cpp
+++ DriversUnit/main.cpp
@@ -10,42 +10,61 @@
 #include <util/delay.h>
 #include <math.h>
 #include "adconverter.h"
-#include "thermometer.h"
 #include "uart.h"
 #include "../common/uartSender.h"
 #include "../common/myString.h"
 #include "../common/packet.h"
 #include "../common/sFloat.h"
+#include "../common/thermometer.h"
 
 
 #define LED_PIN 5
 
-#define CURRENT_SENSOR_1_PIN 6
-#define CURRENT_SENSOR_2_PIN 7
+#define CURRENT_SENSOR_1_CHANNEL 6
+#define CURRENT_SENSOR_2_CHANNEL 7
 
 #define CURRENT_SAMPLES_COUNT 10
 #define CURRENT_SENSOR_COEFFICIENT_x10 357// V -> A
 
+#define TEMP_M1_CHANNEL 0
+#define TEMP_M2_CHANNEL 1
+#define TEMP_D1_CHANNEL 2
+#define TEMP_D2_CHANNEL 3
+#define TEMP_I1_CHANNEL 4
+#define TEMP_I2_CHANNEL 5
+
+#define THERMOMETER_SAMPLES_COUNT 5
+#define LM35_MV_TO_C_COEFF 0.1 //degC per mV
+#define THERMOMETERS_COUNT 6
+
 const int samples = 50;
 
-float convertVoltageToCurrent(float voltage) {
-	float curr = (voltage - 2.5)/CURRENT_SENSOR_COEFFICIENT;
-	return fabs(curr);
-}
-
-
-ADConverter adc;
 
 float current1;
 float current2;
+Thermometer thermometers[THERMOMETERS_COUNT];
+
+void initializeSensors()
+{
+	for(int i=0; i < THERMOMETERS_COUNT; i++) {
+		thermometers[i].setCoefficient(LM35_MV_TO_C_COEFF);
+	}
+}
+
+void readTemperatures()
+{
+	for(int i=0; i < THERMOMETERS_COUNT; i++) {
+		thermometers[i].insertNewReading(ADConverter::getAverageVoltage(i, THERMOMETER_SAMPLES_COUNT) * 1000);
+	}
+}
 
 Packet preparePacket()
 {
 	Packet pack;
 	
 	
-	current1 = convertVoltageToCurrent(adc.getAverageVoltage(CURRENT_SENSOR_1_PIN, samples));
-	current2 = convertVoltageToCurrent(adc.getAverageVoltage(CURRENT_SENSOR_2_PIN, samples));
+	//current1 = convertVoltageToCurrent(adc.getAverageVoltage(CURRENT_SENSOR_1_PIN, samples));
+	//current2 = convertVoltageToCurrent(adc.getAverageVoltage(CURRENT_SENSOR_2_PIN, samples));
 	
 	return pack;
 }
@@ -55,7 +74,8 @@ int main(void)
 	sei();
 	DDRD |= (1<<PD1);
 	uart_init(UART_BAUD_SELECT(9600, F_CPU));
-	
+	ADConverter::init();
+	initializeSensors();
 	uartSender sender;
 	Packet pack;
 	
